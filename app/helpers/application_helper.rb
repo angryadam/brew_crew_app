3a7c4c0e1f22c23515ed0225343a9fe1ad99ed20
angryadam <adam@amyers.io>
2020-12-07 00:10:51 -0800

    Adds footer and helper methods to support it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
   end
 
   def background_image_css
-    default_photo_url = 'https://images.unsplash.com/photo-1505075106905-fb052892c116?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxODg3OTh8MHwxfGFsbHx8fHx8fHx8&ixlib=rb-1.2.1&q=80&w=1080&utm_source=brew-crew-app&utm_medium=referral&utm_campaign=api-credit'
+    default_photo_url = "https://images.unsplash.com/photo-1505075106905-fb052892c116?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxODg3OTh8MHwxfGFsbHx8fHx8fHx8&ixlib=rb-1.2.1&q=80&w=1080&utm_source=#{Rails.application.credentials.unsplash[:utm_source]}&utm_medium=referral&utm_campaign=api-credit"
     # default_photo_url = Unsplash::Photo.find('08tX2fsuSLg').urls.regular
     sanitize("background-image: linear-gradient(to top, rgba(32,26,22,.5), rgba(32,26,22,.5)),
                                 url('#{default_photo_url}');
@@ -24,5 +24,12 @@ module ApplicationHelper
               background-attachment: fixed, fixed;
               background-position: center, center;
               height: 100%;")
+  end
+
+  def footer_credit_links
+    credit = Image.credit
+    unsplash_credit = Image.unsplash_credit
+    sanitize("Background by #{ link_to(credit.name, credit.url, target: :_blank) } " +
+                 " on #{ link_to unsplash_credit.text, unsplash_credit.url, target: :_blank }")
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,21 @@
 class Image < ApplicationRecord
   validates :image_url, presence: true
+
+  def self.credit
+    if false #(image = Unsplash::Photo.find('08tX2fsuSLg'))
+      OpenStruct.new(name: image.user.name,
+                     url: image.user.links.html)
+    else
+      OpenStruct.new(name: 'Christin Hume',
+                     url: "https://unsplash.com/@christinhumephoto?utm_source=#{Rails.application.credentials.unsplash[:utm_source]}" +
+                               '&utm_medium=referral&utm_campaign=api-credit')
+    end
+  end
+
+  def self.unsplash_credit
+    OpenStruct.new(text: 'Unsplash',
+                   url: "https://unsplash.com/?utm_source=#{Rails.application.credentials.unsplash[:utm_source]}" +
+                             '&utm_medium=referral&utm_campaign=api-credit')
+  end
+
 end
