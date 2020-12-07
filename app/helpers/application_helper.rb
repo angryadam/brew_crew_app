9d778715701fc9f985325fe30b52b2231edad6c9
angryadam <adam@amyers.io>
2020-12-06 16:52:52 -0800

    Adds groundwork for dynamic background images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,14 @@ module ApplicationHelper
     datetime.in_time_zone("Pacific Time (US & Canada)")
             .strftime('%b. %e %Y, %l:%M%P')
   end
+
+  def background_image_css
+    default_photo_url = "https://images.unsplash.com/photo-1505075106905-fb052892c116?ixid=MXwxMjA3fDB8MHxzZWFyY2h8ODB8fGJlZXJ8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+    sanitize("background-image: linear-gradient(to top, rgba(32,26,22,.5), rgba(32,26,22,.5)),
+                                url('#{default_photo_url}');
+              background-size: auto, cover;
+              background-attachment: fixed, fixed;
+              background-position: center, center;
+              height: 100%;")
+  end
 end
