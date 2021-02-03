17d8821e5b2ac2ba87fb7e4e8c1904ff15dc6ea1
angryadam <adam@amyers.io>
2021-02-02 23:37:51 -0800

    Adds SMTP settings for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "project_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'advent-beer.herokuapp.com' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'adventbeer.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.zoho.com',
+    port:                 465,
+    domain:               'adventbeer.com',
+    user_name:            Rails.application.credentials.mailer[:username],
+    password:             Rails.application.credentials.mailtrap[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
