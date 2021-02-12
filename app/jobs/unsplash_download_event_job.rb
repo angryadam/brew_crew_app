d22dbded90ba4a908cfdc175fb7e5033fe808d97
angryadam <adam@amyers.io>
2021-02-11 17:41:14 -0800

    Hopefully catches internal Unsplash gem error from stopping Sidekiq

--- app/jobs/unsplash_download_event_job.rb
+++ app/jobs/unsplash_download_event_job.rb
@@ -5,5 +5,8 @@ class UnsplashDownloadEventJob < ApplicationJob
     photo = Unsplash::Photo.find(unsplash_id)
 
     photo.track_download if photo.present?
+  rescue NoMethodError => _
+    # internal gem error was preventing sidekiq job from completing
+    nil
   end
 end
