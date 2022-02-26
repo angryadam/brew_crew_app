1dbcebc6691933d8b36603af31c6bbf09481f058
angryadam <adam@amyers.io>
2022-02-26 10:51:11 -0800

    Reverts node and webpacker versions.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-gem install bundler && bundle install --jobs=3 --retry=3
 bundle check || bundle binstubs --path "$BUNDLE_BIN"
+bundle install --jobs=3
 rm -f /project/tmp/pids/server.pid
 
 exec "$@"
